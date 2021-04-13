4a2b3476a457f9df6ece0866aeac3241f57ea09a
simonapascal98@gmail.com <simonapascal98@gmail.com>
2021-04-13 11:51:50 +0300

    -

--- ProiectPDMXamarin_Login/ProiectPDMXamarin_Login/Pages/MasterPage.xaml.cs
+++ ProiectPDMXamarin_Login/ProiectPDMXamarin_Login/Pages/MasterPage.xaml.cs
@@ -68,7 +68,7 @@ namespace ProiectPDMXamarin_Login.Pages
                         break;
                     case "Profile":
                         {
-                            
+
                             Detail.Navigation.PushAsync(new ProfilePage(user));
                             IsPresented = false;
                         }
